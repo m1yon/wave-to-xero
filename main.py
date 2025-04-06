a82e50cfaa721d65fdb354820a59f56b2965be80
Michael Lyon <mlyon360@gmail.com>
2025-04-06 15:10:42 -0600

    add first four require columns

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+from datetime import timedelta
+
+import polars as pl
+
+
 def main():
-    print("Hello from wave-to-xero!")
+    xero_df = pl.read_csv("xero-template.csv")
+    wave_df = pl.read_csv("wave.ignore.csv")
+    print(xero_df.columns)
+
+    with pl.Config(tbl_cols=-1):
+        print(wave_df)
+
+    result_df = wave_df.select(
+        pl.col("Customer").alias("*ContactName"),
+        pl.col("Invoice Number").alias("*InvoiceNumber"),
+        pl.col("Transaction Date")
+        .str.strptime(pl.Date, format="%m/%d/%y")
+        .alias("*InvoiceDate"),
+        (
+            pl.col("Transaction Date").str.strptime(pl.Date, format="%m/%d/%y")
+            + timedelta(days=30)
+        ).alias("*InvoiceDueDate"),
+    )
+    print(result_df)
 
 
 if __name__ == "__main__":
